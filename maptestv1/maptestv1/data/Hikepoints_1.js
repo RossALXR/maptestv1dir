dc02d322a29a713dea7edd291152542050f239c9
RossALXR <51762631+RossALXR@users.noreply.github.com>
2020-04-12 14:36:43 +0200

    Add files via upload

---
+++ maptestv1/maptestv1/data/Hikepoints_1.js
@@ -0,0 +1,9 @@
+var json_Hikepoints_1 = {
+"type": "FeatureCollection",
+"name": "Hikepoints_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1" }, "geometry": { "type": "Point", "coordinates": [ 18.40323679753206, -33.964948132436248 ] } },
+{ "type": "Feature", "properties": { "fid": "2" }, "geometry": { "type": "Point", "coordinates": [ 18.384962651178643, -33.966955981736355 ] } }
+]
+}
---
+++ maptestv1/maptestv1/data/Hikeroutes_2.js
@@ -0,0 +1,8 @@
+var json_Hikeroutes_2 = {
+"type": "FeatureCollection",
+"name": "Hikeroutes_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1" }, "geometry": { "type": "LineString", "coordinates": [ [ 18.384962651178643, -33.966955981736355 ], [ 18.40323679753206, -33.964948132436248 ] ] } }
+]
+}
